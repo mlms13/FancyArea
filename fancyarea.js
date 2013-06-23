3d46b57074e4f345c62dba0dcf23f0399b18ea91
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-06-23 09:33:21 -0400

    Basic plugin boilerplate

---
+++ fancyarea.js
@@ -0,0 +1,13 @@
+/*! FancyArea */
+// Copyright 2013 Michael Martin-Smucker
+// Released under the MIT License
+
+!function ($) {
+	"use strict";
+
+	$.fn.fancyArea = function () {
+		return this.each(function () {
+			// Make it fancy
+		});
+	};
+}(jQuery);
