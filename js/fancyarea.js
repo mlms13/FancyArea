f9eca8c4dd6ba9b95930834d69493a227e4a8fd4
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-06-30 22:27:03 -0400

    Make some of the remove-item functionality reusable

--- js/fancyarea.js
+++ js/fancyarea.js
@@ -17,15 +17,26 @@
                 $entry = $('<input class="fancy-text-entry" />').appendTo($area),
                 items = [];
 
+            function getCurrentItems() {
+                var strItems = [],
+                    i;
+
+                for (i = 0; i < items.length; i += 1) {
+                    strItems[i] = items[i].text;
+                }
+
+                return strItems;
+            }
+
             function addItem(text) {
                 var $li = $('<li class="fancy-item"></li>').appendTo($list),
                     $item = $('<input value="' + text + '" />').appendTo($li),
-                    $remove = $('<span class="fancy-remove">&times;</span>').appendTo($li);
+                    $remove = $('<span class="fancy-remove">&times;</span>').appendTo($li),
+                    index = items.length;
 
                 $remove.on('click', function (e) {
                     e.stopPropagation();
-                    $li.remove();
-                    $area.trigger('fancyItemChanged', [items]).trigger('fancyItemRemoved', [text]);
+                    removeItem(index);
                     $entry.focus();
                 });
 
@@ -35,20 +46,37 @@
                     // re-focus the original entry after editing is done
                     $entry.focus();
                 }).on('blur', function () {
-                    var $this = $(this),
-                        index = $this.parent().index();
+                    var val = $item.val();
 
                     // if the value didn't change, return
-                    if (items[index] === $this.val()) { return; }
+                    if (items[index].text === val) { return; }
 
                     // otherwise, update the array with the latest data,
                     // and trigger the change function
-                    items[index] = $this.val();
-                    $area.trigger('fancyItemChanged', [items]);
+                    items[index].text = val;
+                    $area.trigger('fancyItemChanged', [getCurrentItems()]);
                 });
 
-                items.push(text);
-                $area.trigger('fancyItemChanged', [items]).trigger('fancyItemAdded', [text]);
+                items.push({
+                    $li: $li,
+                    text: text,
+                    index: index
+                });
+                $area.trigger('fancyItemChanged', [getCurrentItems()]).trigger('fancyItemAdded', [text]);
+            }
+
+            function removeItem(index) {
+                var current = items[index],
+                    text; // = current.text;
+
+                // remove the list item from the page
+                current.$li.remove();
+
+                // remove the index from the items array
+                items.splice(index, 1);
+
+                // trigger the change and remove events with updated data
+                $area.trigger('fancyItemChanged', [getCurrentItems()]).trigger('fancyItemRemoved', [text]);
             }
 
             // absorb classes from the existing textarea that is being replaced
@@ -83,17 +111,15 @@
                     text =  $entry.val();
                     addItem(text);
                     $entry.val('');
-
                 }
             }).on('keydown', function (e) {
-                var $prev;
+                var lastIndex = items.length - 1;
 
                 if (e.which === 8 && $entry.val() === '') {
                     // if backspace is pressed and the input is empty
                     // re-edit the previous item
-                    $prev = $area.find('li:last');
-                    $entry.val($prev.find('input').val());
-                    $prev.find('.fancy-remove').click();
+                    $entry.val(items[lastIndex].text);
+                    removeItem(lastIndex);
                     return false;
                 }
             });
