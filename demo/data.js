baeffb877109f6d2a1c68b6a19d04be8543c9535
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-07-10 23:56:49 -0400

    Render a template for typeahead (fixes #21)

---
+++ demo/data.js
@@ -0,0 +1,250 @@
+var data = [{
+    value: "Alabama",
+    tag: "Sweet Home Alabama",
+    code: "al"
+},
+{
+    value: "Alaska",
+    tag: "North to the Future",
+    code: "ak"
+},
+{
+    value: "Arizona",
+    tag: "The Grand Canyon State",
+    code: "az"
+},
+{
+    value: "Arkansas",
+    tag: "The Natural State",
+    code: "ar"
+},
+{
+    value: "California",
+    tag: "The Golden State",
+    code: "ca"
+},
+{
+    value: "Colorado",
+    tag: "Enter a Higher State",
+    code: "co"
+},
+{
+    value: "Connecticut",
+    tag: "Constitution State",
+    code: "ct"
+},
+{
+    value: "Delaware",
+    tag: "It's Good Being First",
+    code: "de"
+},
+{
+    value: "Florida",
+    tag: "Sunshine State",
+    code: "fl"
+},
+{
+    value: "Georgia",
+    tag: "Georgia on My Mind",
+    code: "ga"
+},
+{
+    value: "Hawaii",
+    tag: "The Islands of Aloha",
+    code: "hi"
+},
+{
+    value: "Idaho",
+    tag: "Great Potatoes. Tasty Destinations.",
+    code: "id"
+},
+{
+    value: "Illinois",
+    tag: "The Land of Lincoln",
+    code: "il"
+},
+{
+    value: "Indiana",
+    tag: "Crossroads of America",
+    code: "in"
+},
+{
+    value: "Iowa",
+    tag: "Life Changing",
+    code: "ia"
+},
+{
+    value: "Kansas",
+    tag: "There's No Place Like Home",
+    code: "ks"
+},
+{
+    value: "Kentucky",
+    tag: "Unbridled Spirit",
+    code: "ky"
+},
+{
+    value: "Louisiana",
+    tag: "",
+    code: "la"
+},
+{
+    value: "Maine",
+    tag: "",
+    code: "me"
+},
+{
+    value: "Maryland",
+    tag: "",
+    code: "md"
+},
+{
+    value: "Massachusetts",
+    tag: "",
+    code: "ma"
+},
+{
+    value: "Michigan",
+    tag: "",
+    code: "mi"
+},
+{
+    value: "Minnesota",
+    tag: "",
+    code: "mn"
+},
+{
+    value: "Mississippi",
+    tag: "",
+    code: "ms"
+},
+{
+    value: "Missouri",
+    tag: "",
+    code: "mo"
+},
+{
+    value: "Montana",
+    tag: "",
+    code: "mt"
+},
+{
+    value: "Nebraska",
+    tag: "",
+    code: "ne"
+},
+{
+    value: "Nevada",
+    tag: "",
+    code: "nv"
+},
+{
+    value: "New Hampshire",
+    tag: "",
+    code: "nh"
+},
+{
+    value: "New Jersey",
+    tag: "",
+    code: "nj"
+},
+{
+    value: "New Mexico",
+    tag: "",
+    code: "nm"
+},
+{
+    value: "New York",
+    tag: "",
+    code: "ny"
+},
+{
+    value: "North Carolina",
+    tag: "",
+    code: "nc"
+},
+{
+    value: "North Dakota",
+    tag: "",
+    code: "nd"
+},
+{
+    value: "Ohio",
+    tag: "",
+    code: "oh"
+},
+{
+    value: "Oklahoma",
+    tag: "",
+    code: "ok"
+},
+{
+    value: "Oregon",
+    tag: "",
+    code: "or"
+},
+{
+    value: "Pennsylvania",
+    tag: "",
+    code: "pa"
+},
+{
+    value: "Rhode Island",
+    tag: "",
+    code: "ri"
+},
+{
+    value: "South Carolina",
+    tag: "",
+    code: "sc"
+},
+{
+    value: "South Dakota",
+    tag: "",
+    code: "sd"
+},
+{
+    value: "Tennessee",
+    tag: "",
+    code: "tn"
+},
+{
+    value: "Texas",
+    tag: "",
+    code: "tx"
+},
+{
+    value: "Utah",
+    tag: "",
+    code: "ut"
+},
+{
+    value: "Vermont",
+    tag: "",
+    code: "vt"
+},
+{
+    value: "Virginia",
+    tag: "",
+    code: "va"
+},
+{
+    value: "Washington",
+    tag: "",
+    code: "wa"
+},
+{
+    value: "West Virginia",
+    tag: "",
+    code: "wv"
+},
+{
+    value: "Wisconsin",
+    tag: "",
+    code: "wi"
+},
+{
+    value: "Wyoming",
+    tag: "",
+    code: "wy"
+}];
